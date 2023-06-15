5b606673d80e7e16b2bfa510ba3f497cb636162e
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-15 18:24:26 +0300

    completed scope lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+     
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'john'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "jack"
+    return leastFavoriteCustomer
+}
